d1aaed65e925f834f195287bbbce1b3a25995069
Rafael Barbosa <rafael.barbosa51@gmail.com>
2024-01-29 19:45:28 -0600

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
+import { Canvas } from "@react-three/fiber"
 function App() {
-  
-
   return (
     <div className='App'>
-      
+      {/* Create the Canvas with a Camera at 0,0,3 */}
+      <Canvas camera={[0,0,3]}>
+
+      </Canvas>
     </div>
   )
 }
-
 export default App
